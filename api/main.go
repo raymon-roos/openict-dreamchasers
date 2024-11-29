9ffff073eeb5f9bd1b26679822ae20ac70d5261d
Edwinvelt <Edwin.v.velthoven@gmail.com>
2024-11-29 09:48:57 +0100

    Fixed the "Aborted connection to db: " Errors
    !Important!
    The fix is to add "?multiStatements=true" when opening the database.
    How it looks in full:
          dsn := "root:4321@tcp(localhost:3306)/dreamchasers?multiStatements=true"

--- api/main.go
+++ api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -11,8 +12,9 @@ import (
 )
 
 func main() {
-	// Open a connection to the database
-	db, err := sql.Open("mysql", "root:4321@tcp(localhost:3306)/dreamchasers")
+	// Open a connection to the database with multiStatements enabled
+	dsn := "root:4321@tcp(localhost:3306)/dreamchasers?multiStatements=true"
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -27,40 +29,44 @@ func main() {
 	m, err := migrate.NewWithDatabaseInstance(
 		"file://migrations",
 		"mysql", driver)
-	m.Up()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Apply migrations
 	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-		log.Fatal(err)
+		log.Printf("Migration error: %v", err)
+		if err == migrate.ErrNoChange {
+			log.Println("No change in migrations")
+		} else {
+			log.Fatal(err)
+		}
 	}
 
 	log.Println("Migrations applied successfully!")
 
-	// 	// Query the database
-	// 	rows, err := db.Query("SELECT id, status_name FROM camp_status")
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	defer rows.Close()
+	// Query the database
+	rows, err := db.Query("SELECT id, status_name FROM camp_status")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
 
-	// 	fmt.Println("hi")
-	// 	// Iterate through the result set
-	// 	for rows.Next() {
-	// 		var id string
-	// 		var campNumber string
-	// 		err := rows.Scan(&id, &campNumber)
-	// 		if err != nil {
-	// 			log.Fatal(err)
-	// 		}
+	fmt.Println("hi")
+	// Iterate through the result set
+	for rows.Next() {
+		var id string
+		var statusName string
+		err := rows.Scan(&id, &statusName)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	// 		fmt.Println(id, campNumber)
-	// 	}
+		fmt.Println(id, statusName)
+	}
 
-	// // Check for errors from iterating over rows
-	//
-	//	if err := rows.Err(); err != nil {
-	//		log.Fatal(err)
-	//	}
+	// Check for errors from iterating over rows
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
