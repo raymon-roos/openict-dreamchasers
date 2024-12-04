1b71fb4ce397859607f11f277344fcac1d92ed59
Raymon Roos <raymon.roos@hotmail.com>
2024-12-04 11:51:15 +0100

    feat: update migrations with design changes & include database docs
    - Adds one-to-many relation for linking "features" to accommodation
      types, like "fully equiped kitchen" for a bungalow.
    - Spelling & naming mistakes in table names and foreign keys.
    - `created_at` `updated_at` `deleted_at` columns, with default values
      where relevant.
    - `house_rules` table, so that these can be modified by the admin. It's
      not intended as a full on CMS, so these records should only hold plain
      text and not HTML, as I don't want to be responsible for preventing
      cross-site scripting.

--- api/migrations/000001_create_db.up.sql
+++ api/migrations/000001_create_db.up.sql
@@ -5,25 +5,30 @@ CREATE TABLE IF NOT EXISTS countries (
 
 CREATE TABLE IF NOT EXISTS addresses (
     id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    country_id VARCHAR(3),
+    country_id VARCHAR(3) NOT NULL,
     postcode VARCHAR(20),
     city VARCHAR(255),
     street VARCHAR(255),
     number INT,
     suffix VARCHAR(10),
+    created_at DATETIME DEFAULT now(),
+    updated_at DATETIME ON UPDATE now(),
+    deleted_at DATETIME,
     FOREIGN KEY (country_id) REFERENCES countries(iso_code)
 );
 
 CREATE TABLE IF NOT EXISTS guests (
     id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255),
+    first_name VARCHAR(255) NOT NULL,
     middle_name VARCHAR(255),
-    last_name VARCHAR(255),
+    last_name VARCHAR(255) NOT NULL,
     date_of_birth DATE,
     email VARCHAR(255) UNIQUE NOT NULL,
-    phone_number VARCHAR(20),
-    address_id BIGINT UNSIGNED,
-    created_at DATETIME NOT NULL,
+    phone_number VARCHAR(20) UNIQUE,
+    address_id BIGINT UNSIGNED NOT NULL,
+    created_at DATETIME DEFAULT now(),
+    updated_at DATETIME ON UPDATE now(),
+    deleted_at DATETIME,
     FOREIGN KEY (address_id) REFERENCES addresses(id)
 );
 
@@ -31,17 +36,32 @@ CREATE TABLE IF NOT EXISTS admins (
     id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    hash_password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT now(),
+    updated_at DATETIME ON UPDATE now(),
+    deleted_at DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS house_rules (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    sort INT UNIQUE NOT NULL,
+    rule_text TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS payment_methods (
     id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) UNIQUE NOT NULL
+    name VARCHAR(255) UNIQUE NOT NULL,
+    created_at DATETIME DEFAULT now(),
+    updated_at DATETIME ON UPDATE now(),
+    deleted_at DATETIME
 );
 
 CREATE TABLE IF NOT EXISTS payment_statuses (
     id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    status VARCHAR(255) NOT NULL
+    status VARCHAR(255) UNIQUE NOT NULL,
+    created_at DATETIME DEFAULT now(),
+    updated_at DATETIME ON UPDATE now(),
+    deleted_at DATETIME
 );
 
 CREATE TABLE IF NOT EXISTS payments (
@@ -49,18 +69,12 @@ CREATE TABLE IF NOT EXISTS payments (
     method_id BIGINT UNSIGNED NOT NULL,
     status_id BIGINT UNSIGNED NOT NULL,
     total_price FLOAT NOT NULL,
-    created_at DATETIME NOT NULL,
     completed_at DATETIME,
+    created_at DATETIME DEFAULT now(),
+    updated_at DATETIME ON UPDATE now(),
+    deleted_at DATETIME,
     FOREIGN KEY (method_id) REFERENCES payment_methods(id),
     FOREIGN KEY (status_id) REFERENCES payment_statuses(id)
-);
-
-CREATE TABLE IF NOT EXISTS accommodation_types (
-    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    type VARCHAR(255) NOT NULL,
-    max_guests INT NOT NULL,
-    price FLOAT NOT NULL,
-    created_at DATETIME NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS accommodations (
@@ -68,7 +82,30 @@ CREATE TABLE IF NOT EXISTS accommodations (
     accommodation_type_id BIGINT UNSIGNED NOT NULL,
     accommodation_number TINYINT UNSIGNED NOT NULL,
     coordinate GEOMETRY NOT NULL,
-    created_at DATETIME NOT NULL,
+    created_at DATETIME DEFAULT now(),
+    updated_at DATETIME ON UPDATE now(),
+    deleted_at DATETIME,
+    FOREIGN KEY (accommodation_type_id) REFERENCES accommodation_types(id)
+);
+
+CREATE TABLE IF NOT EXISTS accommodation_types (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    type VARCHAR(255) UNIQUE NOT NULL,
+    max_guests INT NOT NULL,
+    price FLOAT NOT NULL,
+    created_at DATETIME DEFAULT now(),
+    updated_at DATETIME ON UPDATE now(),
+    deleted_at DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS accommodation_features (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    accommodation_type_id BIGINT UNSIGNED NOT NULL,
+    description VARCHAR(255) UNIQUE NOT NULL,
+    price FLOAT NOT NULL,
+    created_at DATETIME DEFAULT now(),
+    updated_at DATETIME ON UPDATE now(),
+    deleted_at DATETIME,
     FOREIGN KEY (accommodation_type_id) REFERENCES accommodation_types(id)
 );
 
@@ -79,7 +116,10 @@ CREATE TABLE IF NOT EXISTS reservations (
     accommodation_id BIGINT UNSIGNED NOT NULL,
     checkin DATE NOT NULL,
     checkout DATE NOT NULL,
-    created_at DATETIME NOT NULL,
+    created_at DATETIME DEFAULT now(),
+    updated_at DATETIME ON UPDATE now(),
+    deleted_at DATETIME,
+    canceled_at DATETIME,
     FOREIGN KEY (guest_id) REFERENCES guests(id),
     FOREIGN KEY (payment_id) REFERENCES payments(id),
     FOREIGN KEY (accommodation_id) REFERENCES accommodations(id)
@@ -87,8 +127,11 @@ CREATE TABLE IF NOT EXISTS reservations (
 
 CREATE TABLE IF NOT EXISTS price_categories (
     id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    category_name VARCHAR(255) NOT NULL,
-    price FLOAT NOT NULL
+    category_name VARCHAR(255) UNIQUE NOT NULL,
+    price FLOAT NOT NULL,
+    created_at DATETIME DEFAULT now(),
+    updated_at DATETIME ON UPDATE now(),
+    deleted_at DATETIME
 );
 
 CREATE TABLE IF NOT EXISTS line_items (
@@ -96,7 +139,9 @@ CREATE TABLE IF NOT EXISTS line_items (
     payment_id BIGINT UNSIGNED NOT NULL,
     category_id BIGINT UNSIGNED NOT NULL,
     quantity INT NOT NULL,
-    created_at DATETIME NOT NULL,
+    created_at DATETIME DEFAULT now(),
+    updated_at DATETIME ON UPDATE now(),
+    deleted_at DATETIME,
     FOREIGN KEY (payment_id) REFERENCES payments(id),
     FOREIGN KEY (category_id) REFERENCES price_categories(id)
 );
---
+++ docs/database.md
@@ -0,0 +1,192 @@
+---
+lang: nl
+---
+
+## Entity Relation Diagram
+
+In dit diagram is het databasa schema ontwerp uitgebeeld. Een belangrijk doel van het
+ontwerp is om complete persoonsgegevens op te slaan voor administratieve doeleinden en om
+te voldoen aan de wet. Daarnaast moet het mogelijk zijn voor de beheerders van de
+applicatie - Boer Bert zelf, of administratieve medewerkers - om operationele parameters
+aan te passen. Bijvoorbeeld het aanbod van (soorten) accommodaties en de bijbehorende
+(soorten) prijzen. Dit alles met zo min mogelijk duplicatie door middel van normalisatie
+om data inconsistentie te voorkomen. Ook moeten historische gegevens intact blijven onder
+bijvoorbeeld prijswijzigingen.
+
+## Kwaliteitscriteria
+
+- Er is voldaan aan ten minste de vierde normaalvorm [^norm].
+- Alle namen zijn in het Engels, in kleine letters geschreven en met underscores om
+  woorden van elkaar te scheiden (e.g. snake_case).
+- Tabel namen zijn altijd in meervoud.
+- Foreign key kolommen hebben altijd een naam in
+  de vorm van _\<gerelateerde-naam-in-enkelvoud\>\_id_.
+
+[^norm]: https://en.wikipedia.org/wiki/Database_normalization
+
+```mermaid
+erDiagram
+
+accommodations {
+    unsigned_bigint id PK
+    unsigned_bigint accommodation_type_id FK
+    unsigned_tinyint accommodation_number
+    geometry coordinate
+    date created_at
+    date updated_at
+    date deleted_at
+}
+
+accommodation_types {
+    unsigned_bigint id PK
+    string type UK
+    int max_guests
+    float price
+    date created_at
+    date updated_at
+    date deleted_at
+}
+
+accommodation_features {
+    unsigned_bigint id PK
+    unsigned_bigint accommodation_type_id FK
+    string description UK
+    float price
+    date created_at
+    date updated_at
+    date deleted_at
+}
+
+guests {
+    unsigned_bigint id PK
+    string first_name
+    string middle_name
+    string last_name
+    date date_of_birth
+    string email UK
+    string phone_number UK
+    unsigned_bigint address_id FK
+    date created_at
+    date updated_at
+    date deleted_at
+}
+
+addresses {
+    unsigned_bigint id PK
+    varchar(3) country_id FK
+    string postcode
+    string city
+    string street
+    int number
+    string suffix
+    date created_at
+    date updated_at
+    date deleted_at
+}
+
+countries {
+    varchar(3) iso_code PK
+    string name UK
+}
+
+reservations {
+    unsigned_bigint id PK
+    unsigned_bigint guest_id FK
+    unsigned_bigint payment_id FK
+    unsigned_bigint accommodation_id FK
+    date checkin
+    date checkout
+    date created_at
+    date updated_at
+    date deleted_at
+    date canceled_at
+}
+
+payments {
+    unsigned_bigint id PK
+    unsigned_bigint method_id FK
+    unsigned_bigint status_id FK
+    float total_price
+    date completed_at
+    date created_at
+    date updated_at
+    date deleted_at
+}
+
+line_items {
+    unsigned_bigint id PK
+    unsigned_bigint payment_id FK
+    unsigned_bigint category_id FK
+    int quantity
+    date created_at
+    date updated_at
+    date deleted_at
+}
+
+payment_methods {
+    unsigned_bigint id PK
+    string name UK
+    date created_at
+    date updated_at
+    date deleted_at
+}
+
+payment_statuses {
+    unsigned_bigint id PK
+    string status UK
+    date created_at
+    date updated_at
+    date deleted_at
+}
+
+price_categories {
+    unsigned_bigint id PK
+    string category_name UK
+    float price
+    date created_at
+    date updated_at
+    date deleted_at
+}
+
+admins {
+    unsigned_bigint id PK
+    string username UK
+    string email UK
+    string password
+    date created_at
+    date updated_at
+    date deleted_at
+}
+
+house_rules {
+    unsigned_bigint id PK
+    int sort UK
+    text rule_text
+}
+
+guests ||--|{ reservations : "books"
+guests ||--|| addresses : "lives at"
+addresses O|--|| countries : "located in"
+
+reservations ||--|| payments : "must have one"
+reservations ||--|| accommodations : "stays in"
+accommodations O|--|| accommodation_types : "is one of"
+accommodation_features }O--|O accommodation_types : "has"
+
+payments ||--|{ line_items : "includes multiple"
+line_items O|--|| price_categories : "categorized by"
+payments O|--|| payment_methods : "uses"
+payments O|--|| payment_statuses : "has status of"
+```
+
+## Gevolgen voor stakeholders
+
+Gezien de database het hart van de applicatie is, is het ontwerp daarvan bepalend voor
+welke features de applicatie kan ondersteunen. Dit ontwerp is dus van belang voor de
+product owner en diens communicatie naar de klant. Alle gegevens die worden opgeslagen
+moeten ook ergens vandaan komen, namelijk uit de web frontend. Onze UX/UI designers en
+frontend developers moeten dus op de hoogte zijn van welke gegevens er ingevoerd moeten
+worden tijdens het reserveren. Dit brengt voor de frontend ontwikkelaar ook extra
+complexiteit mee, omdat deze gegevens in real-time gevalideerd moeten worden voor een
+goede gebruikerservaring. Persoonsgegevens zoals e-mailadressen en internationale
+telefoonnummers en woonadressen/postcodes kunnen lastig zijn om te valideren.
