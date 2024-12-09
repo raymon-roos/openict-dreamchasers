66b0c3c6c0e684ef6a8048955c4b760602866910
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2024-12-10 00:12:56 +0100

    Carousel Code optimalisatie

--- frontend/pages/reserveren/scripts/dotsNavigation.js
+++ frontend/pages/reserveren/scripts/dotsNavigation.js
@@ -1,5 +1,4 @@
 let currentIndex = 0;
-
 const images = document.querySelectorAll(".camping-img");
 const dotsNavigation = document.querySelector(".dots-navigation");
 
@@ -16,45 +15,47 @@ const createDots = () => {
 
 const showImage = (index) => {
   currentIndex = (index + images.length) % images.length;
-  images.forEach((img, i) => {
-    img.style.display = i === currentIndex ? "block" : "none";
-  });
+
+  images.forEach(
+    (img, i) => (img.style.display = i === currentIndex ? "block" : "none")
+  );
+
   updateDots();
 };
 
 const updateDots = () => {
   const dots = dotsNavigation.querySelectorAll(".dot");
+
   dots.forEach((dot, i) => {
+    dot.classList.toggle("active", i === currentIndex); // Actieve dot markeren
     dot.style.display =
       Math.abs(i - currentIndex) < 3 ? "inline-block" : "none"; // Max 3 dots zichtbaar
-    dot.classList.toggle("active", i === currentIndex); // Actieve dot markeren
   });
 };
 
-document
-  .querySelector(".next")
-  .addEventListener("click", () => showImage(currentIndex + 1));
+const changeImage = (increment) => {
+  showImage(currentIndex + increment);
+};
+
+// Event listeners voor navigatie knoppen
+document.querySelector(".next").addEventListener("click", () => changeImage(1));
 document
   .querySelector(".prev")
-  .addEventListener("click", () => showImage(currentIndex - 1));
+  .addEventListener("click", () => changeImage(-1));
 
-// Initialiseren
+// Initialiseren van het dot systeem en het tonen van de eerste afbeelding
 createDots();
 showImage(currentIndex);
-// Selecteer alle tabs
+
+// Event listeners voor tab navigatie
 document.querySelectorAll(".tab").forEach((tab) => {
   tab.addEventListener("click", () => {
-    // Verwijder 'actief' van alle tabs en inhoud
     document
-      .querySelectorAll(".tab")
-      .forEach((t) => t.classList.remove("activeTab"));
-    document
-      .querySelectorAll(".content")
-      .forEach((content) => content.classList.remove("active-content"));
-
-    // Voeg 'actief' toe aan de geklikte tab en bijbehorende inhoud
+      .querySelectorAll(".tab, .content")
+      .forEach((el) => el.classList.remove("activeTab", "active-content"));
     tab.classList.add("activeTab");
-    const contentId = tab.getAttribute("data-content");
-    document.getElementById(contentId).classList.add("active-content");
+    document
+      .getElementById(tab.getAttribute("data-content"))
+      .classList.add("active-content");
   });
 });
