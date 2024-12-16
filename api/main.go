e155c5c4e79a8d02ce357eb57c0f76140babadb8
Raymon Roos <raymon.roos@hotmail.com>
2024-12-16 11:26:24 +0100

    feat(api): put db migration & http server behind cli flags
    This way, when starting the application, you can choose whether you want
    to run the migrations, or start the HTTP server, or both.

--- api/main.go
+++ api/main.go
@@ -3,19 +3,49 @@ package main
 import (
 	"dreamchasers/routes"
 	"dreamchasers/services"
+	"flag"
 	"log"
-
-	"strings"
-
-	// "dreamchasers/services"
-
 	"net/http"
+	"strings"
 )
 
 func main() {
+	shouldMigrate, shouldServe := parseCLIFlags()
+	if *shouldMigrate {
+		println("Beginning database migration")
+		migrate()
+	}
+	if *shouldServe {
+		println("Starting HTTP server")
+		serve()
+	}
+}
+
+func parseCLIFlags() (*bool, *bool) {
+	migrateFlag := flag.Bool("migrate", false, "Run database migrations. Warning, can be destructive!")
+	serveFlag := flag.Bool("serve", false, "Start HTTP server")
+
+	flag.Parse()
+
+	return migrateFlag, serveFlag
+}
+
+func serve() {
+	handlerList := routes.ListHandlerMethods()
+	routeList := routes.GenerateRoutesFromHandlers(handlerList)
+
+	// Sends all request to ExecuteRouteHandler to match a handler to the requested route
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := strings.ToLower(r.URL.Path[1:]) // Remove leading slash
+		routes.ExecuteRouteHandler(routeList, path, w, r)
+	})
+
+	http.ListenAndServe(":4000", nil)
+}
+
+func migrate() {
 	envList := services.LoadEnv()
 
-	// ==== -- -- DB -- -- ====
 	db, err := services.InitializeDatabase(envList)
 	if err != nil {
 		log.Fatal(err)
@@ -27,17 +57,4 @@ func main() {
 	db.SetConnMaxLifetime(0)
 
 	services.DbMigration(db)
-
-	// ==== HTTP ROUTING ====
-	handlerList := routes.ListHandlerMethods()
-
-	routeList := routes.GenerateRoutesFromHandlers(handlerList)
-
-	// In short sends all request to CallHandler. Needed for dynamic routing
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		path := strings.ToLower(r.URL.Path[1:]) // Remove leading slash
-		routes.ExecuteRouteHandler(routeList, path, w, r)
-	})
-
-	http.ListenAndServe(":4000", nil)
 }
