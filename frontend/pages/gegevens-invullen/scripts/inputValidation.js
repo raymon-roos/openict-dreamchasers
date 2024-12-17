3221b72c865fe23bd7a1367e5db3d8146eb9d417
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2024-12-18 00:13:55 +0100

    Persoonlijke gegevens validatie

---
+++ frontend/pages/gegevens-invullen/scripts/inputValidation.js
@@ -0,0 +1,71 @@
+const validator = new JustValidate("#bookingForm");
+
+validator
+  .addField("#firstName", [
+    {
+      rule: "required",
+      errorMessage: "Vul je naam in.",
+    },
+  ])
+  .addField("#lastName", [
+    {
+      rule: "required",
+      errorMessage: "Vul je achternaam in.",
+    },
+  ])
+  .addField("#email", [
+    {
+      rule: "required",
+      errorMessage: "Vul je email in.",
+    },
+    {
+      rule: "email",
+      errorMessage: "Email is niet geldig.",
+    },
+  ])
+  .addField("#confirm-email", [
+    {
+      rule: "required",
+      errorMessage: "Bevestig je e-mailadres.",
+    },
+    {
+      validator: (value, fields) => {
+        // Haal waarde van het eerste veld op
+        const emailField = fields["#email"];
+        if (emailField && emailField.isValid) {
+          return value === emailField.elem.value; // Vergelijk alleen bij geldige waarde
+        }
+        return true; // Skip validatie als eerste veld nog niet geldig is
+      },
+      errorMessage: "E-mailadressen komen niet overeen.",
+    },
+  ])
+  .addField("#dob", [
+    {
+      rule: "required",
+      errorMessage: "Geboortedatum is verplicht.",
+    },
+    {
+      validator: (value) => {
+        // Minimum leeftijd validatie (bijvoorbeeld 18 jaar)
+        const minAge = 18;
+        const birthDate = new Date(value);
+        const today = new Date();
+        const age = today.getFullYear() - birthDate.getFullYear();
+        const month = today.getMonth() - birthDate.getMonth();
+
+        // Als de leeftijd minder is dan de minimumleeftijd
+        if (age < minAge || (age === minAge && month < 0)) {
+          return false;
+        }
+        return true;
+      },
+      errorMessage: "Je moet minimaal 18 jaar oud zijn.",
+    },
+  ])
+  .addField("#terms", [
+    {
+      rule: "required",
+      errorMessage: "Ga akkoord om verder te gaan.",
+    },
+  ]);
