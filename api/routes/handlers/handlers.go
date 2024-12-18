8a847868ef888bc651c24c23be211b50945846da
Raymon Roos <raymon.roos@hotmail.com>
2024-12-18 12:57:18 +0100

    hint voor Isa
    Enjoy :)

--- api/routes/handlers/handlers.go
+++ api/routes/handlers/handlers.go
@@ -15,14 +15,9 @@ func (h Handler) GetOla(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(secret))
 }
 
-func (h Handler) PostOla(w http.ResponseWriter, r *http.Request) {
-	secret := "uuuhhhh.... hi?"
-	fmt.Println(secret)
-	w.Write([]byte(secret))
-}
-
-// TEST function | Just remove this when done
-func (h Handler) DeleteOlaBolla(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Removed Ola (ㆆ_ㆆ)")
-	w.Write([]byte("ㆆ_ㆆ"))
+func (h Handler) PostReservation(w http.ResponseWriter, r *http.Request) {
+	// TODO @Isa, hier gaat een verzoek van de fronten binnen komen, met json data.
+	// Kan jij de data uit die json halen en inserten in de database?
+	// Waarschijnlijk hep je iets zoals "json marshaling" nodig. Alle structs zijn al
+	// gedefinieerd onder het mapje `models/`. Succes :D
 }
