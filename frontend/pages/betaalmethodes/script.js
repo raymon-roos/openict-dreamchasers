177fcd44ff3fc2d7d9b8a8e24cd1acbeb028785e
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2025-01-15 23:48:50 +0100

    betaalmethodes page header responsive

--- frontend/pages/betaalmethodes/script.js
+++ frontend/pages/betaalmethodes/script.js
@@ -1,4 +1,10 @@
 import { renderOverviewCard } from "../../components/overviewCard.js";
+import { createHeader } from "../../components/headerComponent.js";
+
+// Add header component to the DOM
+const container = document.querySelector(".container");
+const header = createHeader();
+container.insertBefore(header, container.querySelector(".mainContainer"));
 
 // Initial Render OverviewCard
 renderOverviewCard("overviewCard");
