e5f1669d9112ceb8acfe04ed16d6fac2dcf23996
Raymon Roos <raymon.roos@hotmail.com>
2025-01-20 15:48:19 +0100

    feat(db/migrations): make `guests`.`date_of_birth` not null

--- api/migrations/000001_create_db.up.sql
+++ api/migrations/000001_create_db.up.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS guests (
     first_name VARCHAR(255) NOT NULL,
     middle_name VARCHAR(255),
     last_name VARCHAR(255) NOT NULL,
-    date_of_birth DATE,
+    date_of_birth DATE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     phone_number VARCHAR(20) UNIQUE,
     address_id BIGINT UNSIGNED NOT NULL,
